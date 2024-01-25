007559fba153602da05515fbdd474b701ab1b33a
ifeoluwaayo <adeayomide112@gmail.com>
2024-01-25 14:25:52 +0100

    gg

--- src/index.ts
+++ src/index.ts
@@ -108,5 +108,5 @@ app.post("/login", (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is listening to you bro from ass ${PORT}...`);
+  console.log(`Server is listening to you bro from ${PORT}...`);
 });
